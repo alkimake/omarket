e65a74035992fc8473f4275b68a897790e0af49d
Alkim Gozen <alkimake@gmail.com>
2019-06-15 16:05:21 +0900

    [lint] spaces are used

--- contracts/OMarket.sol
+++ contracts/OMarket.sol
@@ -3,23 +3,23 @@ pragma solidity >=0.4.21 <0.6.0;
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract OMarket is Ownable {
-	address[] admins;
-	event AdminAdded(address adminAddress);
+  address[] admins;
+  event AdminAdded(address adminAddress);
 
-	function addAdmin(address adminAddress)
-		public
-		onlyOwner
-	{
-		admins.push(adminAddress);
-		emit AdminAdded(adminAddress);
-	}
+  function addAdmin(address adminAddress)
+    public
+    onlyOwner
+  {
+    admins.push(adminAddress);
+    emit AdminAdded(adminAddress);
+  }
 
-	function getAdmins()
-		public
-		view
-		onlyOwner
-		returns(address[] memory)
-	{
-		return admins;
-	}
+  function getAdmins()
+    public
+    view
+    onlyOwner
+    returns(address[] memory)
+  {
+    return admins;
+  }
 }
--- test/oMarket.test.js
+++ test/oMarket.test.js
@@ -1,38 +1,38 @@
 var OMarket = artifacts.require('OMarket')
 let catchRevert = require("./exceptionsHelpers.js").catchRevert
 
-contract('OMarket', function(accounts) {
+contract('OMarket', function (accounts) {
 
-    const deployAccount = accounts[0]
-    const adminAccount = accounts[1]
-    const sellerAccount = accounts[2]
-	const customerAccount = accounts[3]
+  const deployAccount = accounts[0]
+  const adminAccount = accounts[1]
+  const sellerAccount = accounts[2]
+  const customerAccount = accounts[3]
 
-	let instance
+  let instance
 
-    beforeEach(async () => {
-        instance = await OMarket.new()
-	})
+  beforeEach(async () => {
+    instance = await OMarket.new()
+  })
 
-	describe("Setup", async() => {
+  describe("Setup", async () => {
 
-        it("OWNER should be set to the deploying address", async() => {
-            const owner = await instance.owner()
-            assert.equal(owner, deployAccount, "the deploying address should be the owner")
-        })
-	})
+    it("OWNER should be set to the deploying address", async () => {
+      const owner = await instance.owner();
+      assert.equal(owner, deployAccount, "the deploying address should be the owner")
+    })
+  })
 
-	describe("Admin", async() => {
-		describe("addAdmin()", async() => {
-			it("only the owner should be able to add an admin", async() => {
-				await instance.addAdmin(adminAccount, {from: deployAccount});
-				await catchRevert(instance.addAdmin(adminAccount, {from: adminAccount}));
-			});
-			it("adding an admin should triggger an event", async() => {
-				const tx = await instance.addAdmin(adminAccount, {from: deployAccount});
-				const eventData = tx.logs[0].args;
-				assert.equal(eventData.adminAddress, adminAccount, "added admin address should match");
-			});
-		});
-	});
+  describe("Admin", async () => {
+    describe("addAdmin()", async () => {
+      it("only the owner should be able to add an admin", async () => {
+        await instance.addAdmin(adminAccount, { from: deployAccount });
+        await catchRevert(instance.addAdmin(adminAccount, { from: adminAccount }));
+      });
+      it("adding an admin should triggger an event", async () => {
+        const tx = await instance.addAdmin(adminAccount, { from: deployAccount });
+        const eventData = tx.logs[0].args;
+        assert.equal(eventData.adminAddress, adminAccount, "added admin address should match");
+      });
+    });
+  });
 })
