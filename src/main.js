c53a1f39acf33c6c8276095991b11b15bb629736
Alkim Gozen <alkimake@gmail.com>
2019-06-16 12:11:43 +0900

    introduce basic vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
+
+Vue.config.devtools = true
+Vue.config.productionTip = false
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+  	increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
+
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
