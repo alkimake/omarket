dfcb6179655e34be7f9086501adbb3aec643767f
Alkim Gozen <alkimake@gmail.com>
2019-06-30 16:40:29 +0900

    [contract] remove admin account

--- contracts/OMarket.sol
+++ contracts/OMarket.sol
@@ -5,6 +5,7 @@ import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 contract OMarket is Ownable {
   address[] admins;
   event AdminAdded(address adminAddress);
+  event AdminRemoved(address adminAddress);
 
   function addAdmin(address adminAddress)
     public
@@ -12,6 +13,49 @@ contract OMarket is Ownable {
   {
     admins.push(adminAddress);
     emit AdminAdded(adminAddress);
+  }
+
+  function isAdmin(address adminAddress)
+    private
+    view
+    returns(bool)
+  {
+    uint adminLength = admins.length;
+    for (uint i = 0; i < adminLength; i++) {
+      if (admins[i] == adminAddress) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function isAdmin()
+    public
+    view
+    returns(bool)
+  {
+    return isAdmin(msg.sender);
+  }
+
+  function removeAdmin(address adminToBeDeleted)
+    public
+    onlyOwner
+  {
+    require(isAdmin(adminToBeDeleted), "The address is not admin");
+    uint indexToBeDeleted;
+    uint adminLength = admins.length;
+    for (uint i = 0; i < adminLength; i++) {
+      if (admins[i] == adminToBeDeleted) {
+        indexToBeDeleted = i;
+        break;
+      }
+    }
+    if (indexToBeDeleted < adminLength-1 ) {
+      admins[indexToBeDeleted] = admins[adminLength - 1];
+    }
+    delete admins[adminLength - 1];
+    admins.length--;
+    emit AdminRemoved(adminToBeDeleted);
   }
 
   function getAdmins()
--- test/oMarket.test.js
+++ test/oMarket.test.js
@@ -40,5 +40,39 @@ contract('OMarket', function (accounts) {
       assert.equal(admins.length, 1);
       assert.equal(admins[0], adminAccount);
     });
+    it("user should get if user is admin", async() => {
+      const ownerIsAdmin = await instance.isAdmin({from: deployAccount});
+      assert.equal(ownerIsAdmin, false);
+      await instance.addAdmin(adminAccount, {from: deployAccount});
+      const adminIsAdmin = await instance.isAdmin({from: adminAccount});
+      assert.equal(adminIsAdmin, true);
+    });
+    describe("removeAdmin()", async () => {
+      it("should not remove admin if the address is not in admin list", async() => {
+        await catchRevert(instance.removeAdmin(adminAccount, { from: adminAccount }))
+      });
+      it("should remove admin properly", async() => {
+        await instance.addAdmin(adminAccount, {from: deployAccount});
+        const tx = await instance.removeAdmin(adminAccount, {from: deployAccount});
+        const eventData = tx.logs[0].args;
+        assert.equal(eventData.adminAddress, adminAccount, "removed admin adresss should match");
+        const adminList = await instance.getAdmins({from: deployAccount});
+        assert.equal(adminList.length, 0, "Admin list should be empty when removed only admin");
+      });
+      it("should remove admin in the middle", async() => {
+        await instance.addAdmin(adminAccount, {from:deployAccount});
+        await instance.addAdmin(sellerAccount, {from: deployAccount});
+        await instance.addAdmin(customerAccount, {from: deployAccount});
+
+        const tx = await instance.removeAdmin(sellerAccount, {from: deployAccount});
+        const eventData = tx.logs[0].args;
+        assert.equal(eventData.adminAddress, sellerAccount, "removed admin adresss should match");
+
+        const adminList = await instance.getAdmins({from: deployAccount});
+        const admins = [adminAccount, customerAccount];
+        assert.sameMembers(adminList, admins, "rest admin accounts should match");
+
+      });
+    });
   });
 })
