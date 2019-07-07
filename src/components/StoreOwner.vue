1d7776b5b302539bdfc7f4a02429feae4df232fe
Alkim Gozen <alkimake@gmail.com>
2019-07-07 16:20:14 +0900

    [UI] introduced store owner page

--- src/components/StoreOwner.vue
+++ src/components/StoreOwner.vue
@@ -1,5 +1,66 @@
 <template>
-  <div>
-    Store Owner
+  <div class="page-content">
+    <el-form
+      :inline="true"
+      :model="newStoreForm"
+    >
+      <el-form-item label="Store Name">
+        <el-input
+          v-model="newStoreForm.name"
+          placeholder="Name"
+        ></el-input>
+      </el-form-item>
+      <el-select
+        v-model="newStoreForm.labels"
+        multiple
+        filterable
+        allow-create
+        default-first-option
+        placeholder="Choose labels for your store"
+      >
+        <el-option
+          v-for="item in newStoreForm.label.options"
+          :key="item"
+          :label="item"
+          :value="item"
+        >
+        </el-option>
+      </el-select>
+      <el-form-item>
+        <el-button
+          type="primary"
+          icon="el-icon-circle-plus-outline"
+          @click="addNewStore"
+        >
+          Add New Store
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      newStoreForm: {
+        label: {
+          options: [
+            'Food',
+            'Clothes',
+            'Home',
+            'Electronics'
+          ]
+        }
+      },
+    }
+  },
+  methods: {
+    async addNewStore() {
+      console.log(this.newStoreForm.name, this.newStoreForm.labels.join(','));
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
