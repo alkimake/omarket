e1403577dff569647a3e976e7dea4184b803a9f6
Alkim Gozen <alkimake@gmail.com>
2019-07-07 16:49:48 +0900

    [contract] introduced product on store

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -6,22 +6,49 @@ import { strings } from "./lib/strings.sol";
 contract Store is Ownable {
     using strings for *;
 
-	string[] public labels;
-	string public name;
+  string[] public labels;
+  string public name;
 
-	constructor (address storeOwner, string memory _name, string memory _labelsSeperatedByCommas)
-		public
-	{
-		name = _name;
-		strings.slice memory s = _labelsSeperatedByCommas.toSlice();
-		strings.slice memory delim = ",".toSlice();
-		string[] memory labels_ = new string[](s.count(delim) + 1);
-		for(uint i = 0; i < labels_.length; i++) {
-			labels_[i] = s.split(delim).toString();
-		}
-		labels = labels_;
-		transferOwnership(storeOwner);
-	}
+  uint public idGenerator;
+
+
+  struct Product {
+    string name;
+    string description;
+    string imageURL;
+    uint totalStock;
+    uint sales;
+    bool isAvailable;
+    mapping(address => uint) buyers;
+  }
+
+  mapping (uint => Product) products;
+
+  event LogProductAdded(string name, string desc, string imageURL, uint totalStock, uint productId);
+
+  constructor (address storeOwner, string memory _name, string memory _labelsSeperatedByCommas)
+    public
+  {
+    name = _name;
+    strings.slice memory s = _labelsSeperatedByCommas.toSlice();
+    strings.slice memory delim = ",".toSlice();
+    string[] memory labels_ = new string[](s.count(delim) + 1);
+    for(uint i = 0; i < labels_.length; i++) {
+      labels_[i] = s.split(delim).toString();
+    }
+    labels = labels_;
+    transferOwnership(storeOwner);
+  }
+
+  function addProduct(string memory name_, string memory desc, string memory image, uint stock)
+    public
+    onlyOwner
+    returns(uint)
+  {
+    products[idGenerator++] = Product(name_, desc, image, stock, 0, true);
+    emit LogProductAdded(name_, desc, image, stock, idGenerator);
+    return idGenerator;
+  }
 
 
 }
--- test/store.test.js
+++ test/store.test.js
@@ -9,9 +9,30 @@ contract('Store', function (accounts) {
   const customerAccount = accounts[3]
 
   const STORE_NAME = 'Store#1';
-  const STORE_LABELS = 'food,burger,potatoes';
+  const STORE_LABELS = 'food,burger,electronics';
 
   let instance
+
+  const p_potato = {
+    name: 'potato',
+    description: 'delicious potatoes',
+    imageURL: 'http://',
+    totalStock: 5
+  }
+
+  const p_tomato = {
+    name: 'tomato',
+    description: 'red tomatoes',
+    imageURL: 'http://',
+    totalStock: 10
+  }
+
+  const p_corn = {
+    name: 'corn',
+    description: 'long corns',
+    imageURL: 'http://',
+    totalStock: 20
+  }
 
   beforeEach(async () => {
     instance = await Store.new(deployAccount, STORE_NAME, STORE_LABELS)
@@ -21,12 +42,30 @@ contract('Store', function (accounts) {
 
     it("OWNER should be set to the deploying address", async () => {
       const owner = await instance.owner();
-	  assert.equal(owner, deployAccount, "the deploying address should be the owner");
-	  const instance2 = await Store.new(sellerAccount, STORE_NAME, STORE_LABELS);
-	  const owner2 = await instance2.owner();
-	  assert.equal(owner2, sellerAccount, "owner can be transferred on initialization");
-    })
+      assert.equal(owner, deployAccount, "the deploying address should be the owner");
+      const instance2 = await Store.new(sellerAccount, STORE_NAME, STORE_LABELS);
+      const owner2 = await instance2.owner();
+      assert.equal(owner2, sellerAccount, "owner can be transferred on initialization");
+    });
 
-  })
+  });
+
+  describe('Adding product', async () => {
+    it("only the owner should be able to add an product", async() => {
+      await instance.addProduct(p_potato.name, p_potato.description, p_potato.imageURL, p_potato.totalStock, {from: deployAccount} )
+      await catchRevert(instance.addProduct(p_potato.name, p_potato.description, p_potato.imageURL, p_potato.totalStock, {from: adminAccount}))
+    });
+
+    it("adding an product should emit an product with the provided product details", async() => {
+      const tx = await instance.addProduct(p_potato.name, p_potato.description, p_potato.imageURL, p_potato.totalStock, {from: deployAccount} )
+      const productData = tx.logs[0].args
+
+      assert.equal(productData.name, p_potato.name, "the added product name should match")
+      assert.equal(productData.desc, p_potato.description, "the added product descriptions should match")
+      assert.equal(productData.imageURL, p_potato.imageURL, "the added product Urls should match")
+      assert.equal(productData.totalStock.toString(10), p_potato.totalStock.toString(10), "the added product total stock should match")
+    });
+
+  });
 
 });
