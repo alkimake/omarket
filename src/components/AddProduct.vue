07701d210aaf4433ec73860d94db402fa7ea4ffb
Alkim Gozen <alkimake@gmail.com>
2019-07-09 22:25:10 +0900

    add product with listing

---
+++ src/components/AddProduct.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <h2>Add New Product to {{ store.name }}</h2>
+    <el-form
+      ref="form"
+      :model="form"
+      label-width="120px"
+    >
+      <el-form-item label="Picture">
+        <input
+          type="file"
+          @change="handleAdd"
+        ></input>
+      </el-form-item>
+      <el-form-item label="Name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="Price">
+        <el-input-number
+          v-model="form.price"
+          :min="1"
+        ></el-input-number> wei
+      </el-form-item>
+      <el-form-item label="Stock">
+        <el-input-number
+          v-model="form.totalStock"
+          :min="1"
+        ></el-input-number>
+      </el-form-item>
+      <el-form-item label="Description">
+        <el-input
+          v-model="form.desc"
+          type="textarea"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="onSubmit"
+        >
+          Create
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+import { read } from 'fs';
+const IPFS = require('ipfs-api');
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001,protocol: 'https' });
+export default {
+  props: {
+    store: Object
+  },
+  data() {
+    return {
+      form: {
+        name: "",
+        desc: "",
+        imageUrl: "",
+        price: 1,
+        totalStock: 1,
+        file: '',
+      },
+    };
+  },
+  computed: {
+    ipfsImageUrl() {
+      return `https://ipfs.infura.io:5001/api/v0/get?arg=${this.form.imageUrl}`;
+    }
+  },
+  methods: {
+    async onSubmit() {
+      await this.$root.storeSend(this.store.addr, 'addProduct', this.form.name, this.form.desc, this.form.imageUrl, this.form.totalStock, this.form.price);
+    },
+    async handleAdd(ev) {
+
+      // const isJPG = file.type === "image/jpeg";
+      // const isPNG = file.type === "image/png";
+      // const isLt2M = file.size / 1024 / 1024 < 2;
+
+      // if (!isJPG && !isPNG) {
+      //   this.$message.error("Product picture must be JPG or PNG format!");
+      //   return;
+      // }
+      // if (!isLt2M) {
+      //   this.$message.error("Product picture size can not exceed 2MB!");
+      //   return;
+      // }
+      const file = ev.target.files[0];
+      const isJPG = file.type === "image/jpeg";
+      const isPNG = file.type === "image/png";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG && !isPNG) {
+        this.$message.error("Product picture must be JPG or PNG format!");
+        return;
+      }
+      if (!isLt2M) {
+        this.$message.error("Product picture size can not exceed 2MB!");
+        return;
+      }
+      const reader = new FileReader(file);
+      reader.onloadend = () => this.uploadImage(reader);
+      reader.readAsArrayBuffer(file);
+
+    },
+    async uploadImage(reader) {
+      // console.log(reader.result);
+      const buffer = await Buffer.from(reader.result);
+      // console.log(buffer);
+      const ipfsHash = await ipfs.add(buffer);
+      console.log(ipfsHash);
+      this.form.imageUrl = ipfsHash[0].hash;
+    }
+  }
+};
+</script>
+<style>
+</style>
--- src/components/StoreDetail.vue
+++ src/components/StoreDetail.vue
@@ -4,124 +4,79 @@
     <el-row :gutter="20">
       <el-col :span="18">
         <h2>Products</h2>
+        <el-table
+          :data="productList"
+          style="width: 100%"
+        >
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <p>Description: {{ props.row.desc }}</p>
+              <p>Available: {{ props.row.available }}</p>
+              <p>Price: {{ props.row.price }}wei</p>
+              <p>Avalable Stock: {{ props.row.stock }}</p>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="Name"
+            prop="name"
+          >
+          </el-table-column>
+          <el-table-column
+            label="Price"
+            prop="price"
+          >
+          </el-table-column>
+        </el-table>
       </el-col>
       <el-col :span="6">
-        <h2>Add New Product</h2>
-        <el-upload
-          class="product-image-uploader"
-          action="https://jsonplaceholder.typicode.com/posts/"
-          :show-file-list="false"
-          :on-success="handleProductImageSuccess"
-          :before-upload="beforeProductUpload"
-        >
-          <img
-            v-if="form.imageUrl"
-            :src="form.imageUrl"
-            class="avatar"
-          />
-          <i
-            v-else
-            class="el-icon-plus product-image-uploader-icon"
-          ></i>
-        </el-upload>
-        <el-form
-          ref="form"
-          :model="form"
-          label-width="120px"
-        >
-          <el-form-item label="Name">
-            <el-input v-model="form.name"></el-input>
-          </el-form-item>
-          <el-form-item label="Price">
-            <el-input-number
-              v-model="form.price"
-              :min="1"
-            ></el-input-number> wei
-          </el-form-item>
-          <el-form-item label="Stock">
-            <el-input-number
-              v-model="form.totalStock"
-              :min="1"
-            ></el-input-number>
-          </el-form-item>
-          <el-form-item label="Description">
-            <el-input
-              v-model="form.desc"
-              type="textarea"
-            ></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              type="primary"
-              @click="onSubmit"
-            >
-              Create
-            </el-button>
-          </el-form-item>
-        </el-form>
+        <add-product :store="store"></add-product>
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
+import AddProduct from './AddProduct.vue';
 export default {
+  components: {
+    AddProduct,
+  },
   props: {
     store: Object
   },
   data() {
     return {
-      form: {
-        name: "",
-        desc: "",
-        imageUrl: "",
-        price: 1,
-        totalStock: 1,
-      },
+      productList: [],
     };
   },
-  methods: {
-    async onSubmit() {},
-    handleProductImageSuccess(res, file) {
-      this.form.imageUrl = URL.createObjectURL(file.raw);
+  watch: {
+    $props: {
+      handler() {
+        this.productList = [];
+        this.refreshList();
+      },
+      deep: true,
+      immediate: true,
     },
-    beforeProductUpload(file) {
-      const isJPG = file.type === "image/jpeg";
-      const isPNG = file.type === "image/png";
-      const isLt2M = file.size / 1024 / 1024 < 2;
-
-      if (!isJPG && !isPNG) {
-        this.$message.error("Product picture must be JPG or PNG format!");
+  },
+  methods: {
+    async refreshList() {
+      const lastId = await this.$root.storeCall(this.store.addr, 'idGenerator');
+      console.log('id', lastId);
+      for(let i = 0; i < lastId; i++) {
+        const item = await this.$root.storeCall(this.store.addr, 'readProduct', i);
+        this.productList[i] = {
+          name: item['0'],
+          desc: item['1'],
+          imageHash: item['2'],
+          stock: parseInt(item['3']),
+          available: item['4'],
+          price: parseInt(item['5'])
+        }
+        console.log(this.productList[i]);
       }
-      if (!isLt2M) {
-        this.$message.error("Product picture size can not exceed 2MB!");
-      }
-      return (isJPG || isPNG) && isLt2M;
     }
   }
 };
 </script>
 <style>
-  .product-image-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .product-image-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .product-image-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 340px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 100%;
-    height: 178px;
-    display: block;
-  }
 </style>
