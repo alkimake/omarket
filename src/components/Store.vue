802b980e333a85d0f99a903a9ef8270ce471247c
Alkim Gozen <alkimake@gmail.com>
2019-07-27 20:23:15 +0900

    added store owner selection on stores

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,63 +1,81 @@
 <template>
-  <el-row :gutter="20">
-    <el-col
-      v-for="(o) in products"
-      :key="o.store+o.id"
-      :xs="24"
-      :sm="3"
-      :lg="6"
+  <div>
+    Select a Store Owner to see their products
+    <el-select
+      v-model="selectedOwner"
+      :loading="storesLoading"
+      placeholder="Select"
     >
-      <el-card
-        shadow="always"
-        class="item"
-        :body-style="{ padding: '0px' }"
-        :disabled="o.stock==0"
+      <el-option
+        v-for="item in storeOwnerList"
+        :key="item.addr"
+        :label="item.name"
+        :value="item.addr"
+        @change="getStoreList"
       >
-        <div
-          slot="header"
-          class="clearfix"
+      </el-option>
+    </el-select>
+    <el-divider></el-divider>
+    <el-row :gutter="20">
+      <el-col
+        v-for="(o) in products"
+        :key="o.store+o.id"
+        :xs="24"
+        :sm="3"
+        :lg="6"
+      >
+        <el-card
+          shadow="always"
+          class="item"
+          :body-style="{ padding: '0px' }"
+          :disabled="o.stock==0"
         >
-          <span>{{ o.name }}</span>
-          <!-- <el-badge :value="o.stock" class="item"></el-badge> -->
-          <el-button
-            v-if="o.stock!=0"
-            style="float: right;"
-            type="primary"
-            @click="buyItem(o)"
+          <div
+            slot="header"
+            class="clearfix"
           >
-            Buy
-          </el-button>
-          <el-button
-            v-else
-            style="float: right;"
-            type="info"
-            disabled
-          >
-            Out of Stock
-          </el-button>
-        </div>
-        <el-image
-          :src="$root.ipfsAddress(o.imageHash)"
-          class="image"
-          fit="cover"
-        >
-        </el-image>
-        <div style="padding: 14px;">
-          <div class="bottom clearfix">
-            <p class="price">
-              {{ ethPrice(o.price) }} Ξ
-            </p>
-            <p class="desc">
-              {{ o.desc }}
-            </p>
-            <p class="stock">
-              Available: {{ o.stock }}
-            </p>
+            <span>{{ o.name }}</span>
+            <!-- <el-badge :value="o.stock" class="item"></el-badge> -->
+            <el-button
+              v-if="o.stock!=0"
+              style="float: right;"
+              type="primary"
+              @click="buyItem(o)"
+            >
+              Buy
+            </el-button>
+            <el-button
+              v-else
+              style="float: right;"
+              type="info"
+              disabled
+            >
+              Out of Stock
+            </el-button>
           </div>
-        </div>
-      </el-card>
-    </el-col>
-  </el-row>
+          <el-image
+            :src="$root.ipfsAddress(o.imageHash)"
+            class="image"
+            fit="cover"
+          >
+          </el-image>
+          <div style="padding: 14px;">
+            <div class="bottom clearfix">
+              <p class="price">
+                {{ ethPrice(o.price) }} Ξ
+              </p>
+              <p class="desc">
+                {{ o.desc }}
+              </p>
+              <p class="stock">
+                Available: {{ o.stock }}
+              </p>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 
@@ -111,21 +129,45 @@ export default {
       currentDate: new Date(),
       stores: [],
       products: [],
+      storesLoading: false,
+      storeOwnerList: [],
+      selectedOwner: "",
     };
   },
+  watch: {
+    selectedOwner: function (owner) {
+      this.getStoreList(owner)
+    }
+  },
   mounted() {
-    this.getStoreList();
+    this.getStoreOwners();
   },
   methods: {
-    async getStoreList() {
+    async getStoreOwners() {
+      this.storesLoading = true;
+      const list = await this.$root.contractCall('getStoreOwners');
+      this.storeOwnerList = await Promise.all(list.map(async addr => {
+        const result = await this.$root.contractCall('readStoreOwner', addr);
+        const owner = { addr: result['0'], name: result['1'], isActive: result['2']};
+        return owner;
+      }));
+      if (this.storeOwnerList.length > 0) {
+        this.selectedOwner = this.storeOwnerList[0].addr;
+      }
+      this.storesLoading = false;
+    },
+    async getStoreList(owner) {
+      console.log("getStoreList", owner);
       this.products = [];
-      this.stores = await this.$root.contractCall('getStores');
+      this.stores = await this.$root.contractCall('getStoresOfOwner', owner);
+      console.log(this.stores);
       if (this.stores == null) {
         this.stores = [];
       }
       for (const store of this.stores) { this.getProducts(store); }
     },
     async getProducts(store) {
+      console.log("getProducts", store);
       const list = [];
       const lastId = await this.$root.storeCall(store, 'idGenerator');
       for(let i = 0; i < lastId; i++) {
